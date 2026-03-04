75d55231f31bf7b57c65f4aa6de8cc868f53d6b5
fini0 <fini0013@gmail.com>
2026-03-04 11:35:03 +0100

    git init

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @var db $db
+ */
+
+require "settings/init.php";
+?>
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+    
+    <title>Sigende titel</title>
+    
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+    
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+<nav class="navbar bg-body-tertiary position-relative">
+    <div class="position-absolute top-50 start-50 translate-middle">
+        <img src="img/blomsterlogo.png" height="70"</img>
+    </div>
+    <div class="ms-auto pe-3">
+        <a class="navbar-brand fa-solid fa-basket-shopping" href="#"></a>
+    </div>
+</nav>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://kit.fontawesome.com/fa23e7aba1.js" crossorigin="anonymous"></script>
+</body>
+</html>
