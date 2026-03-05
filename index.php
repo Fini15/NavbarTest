f1342627ec362c458b76ed667a19cd1d22be8960
mathias08011 <larsen0801@gmail.com>
2026-03-05 11:04:37 +0100

    move to new doc

--- index.php
+++ index.php
@@ -21,9 +21,7 @@ require "settings/init.php";
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
-
 <body>
-
 <nav class="navbar bg-body-tertiary position-relative">
     <div class="position-absolute top-50 start-50 translate-middle">
         <img src="img/blomsterlogo1.svg" height="50" </img>
@@ -34,19 +32,6 @@ require "settings/init.php";
 </nav>
 
 
-<?php
-$items = [
-        ["id"=>1,"name"=>"Item 1"],
-        ["id"=>2,"name"=>"Item 2"],
-        ["id"=>3,"name"=>"Item 3"],
-        ["id"=>4,"name"=>"Item 4"],
-        ["id"=>5,"name"=>"Item 5"],
-        ["id"=>6,"name"=>"Item 6"],
-        ["id"=>7,"name"=>"Item 7"],
-        ["id"=>8,"name"=>"Item 8"],
-        ["id"=>9,"name"=>"Item 9"],
-];
-?>
 
 
 
@@ -55,65 +40,12 @@ $items = [
 
 
 
-<div class="container mt-4">
-    <div class="row g-3">
 
-        <?php foreach($items as $item): ?>
 
-            <div class="col-4">
-                <div class="cart-box">
 
-                    <div class="item-name">
-                        <?php echo $item['name']; ?>
-                    </div>
 
-                    <div class="controls d-flex align-items-center">
 
-                        <button class="btn btn-outline-danger btn-sm minus" data-id="<?php echo $item['id']; ?>">−</button>
 
-                        <div class="quantity" id="qty-<?php echo $item['id']; ?>">0</div>
-
-                        <button class="btn btn-outline-success btn-sm plus" data-id="<?php echo $item['id']; ?>">+</button>
-
-                    </div>
-
-                </div>
-            </div>
-
-        <?php endforeach; ?>
-
-    </div>
-</div>
-
-<script>
-
-    document.querySelectorAll(".plus").forEach(btn=>{
-        btn.addEventListener("click",function(){
-
-            let id = this.dataset.id;
-            let qty = document.getElementById("qty-"+id);
-
-            qty.innerText = parseInt(qty.innerText) + 1;
-
-        });
-    });
-
-    document.querySelectorAll(".minus").forEach(btn=>{
-        btn.addEventListener("click",function(){
-
-            let id = this.dataset.id;
-            let qty = document.getElementById("qty-"+id);
-
-            let value = parseInt(qty.innerText);
-
-            if(value > 0){
-                qty.innerText = value - 1;
-            }
-
-        });
-    });
-
-</script>
 
 
 
